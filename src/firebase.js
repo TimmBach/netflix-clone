f9e6443bab1eaa613eb1bee95018c8706e2a6d22
TimmBach <follow.timothy@gmail.com>
2021-04-02 01:37:38 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDXbZuR9gwRhDDeVzQeSzX-_INCGrDDBNc",
+//     authDomain: "netflix-clone-a028a.firebaseapp.com",
+//     projectId: "netflix-clone-a028a",
+//     storageBucket: "netflix-clone-a028a.appspot.com",
+//     messagingSenderId: "913060447405",
+//     appId: "1:913060447405:web:9f646c370e1302e9fcf224",
+//     measurementId: "G-46DYMKB88S"
+//   };
---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// API KEY: 79ea5d2e8010cdc56902b3957f70465a
+
+const API_KEY = "79ea5d2e8010cdc56902b3957f70465a";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}with_genres=99`,
+};
+
+export default requests;
